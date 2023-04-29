d46aa31cafcfffba27ac66ab438129dac8827389
Tristan Maindron <tmaindron@gmail.com>
2023-04-29 17:41:28 +0200

    Added trajectory prediction

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -9,36 +9,69 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var was_on_floor = 0.0
 var sliding = false
 var slide_jump = false
-var shoot_power = 500.0
+var max_shoot_power = 800.0
+var shoot_power = 200.0
+var initial_shoot_power = 200.0
 var parcel = preload("res://prefabs/parcel.tscn");
+var load_speed = 250.0
 
 func _ready():
 	$AnimatedSprite2D.play()
 
+func update_trajectory(delta, initial_velocity):
+	var line = $Line2D
+	var max_points = 400
+
+	line.clear_points()
+	var pos = Vector2.ZERO
+	var vel = initial_velocity
+
+	for i in max_points:
+		line.add_point(pos)
+		vel.y += 0.55 * gravity * delta
+		pos += vel * delta
+
+		var query = PhysicsPointQueryParameters2D.new()
+		query.position = transform.get_origin() + pos
+		query.collision_mask = 0b00000000000000000100
+
+		var intersections = get_world_2d().get_direct_space_state().intersect_point(query);
+		if len(intersections) > 0:
+			break
+
+
 func _physics_process(delta):
 	Engine.time_scale = 1.0
 
+	if not is_on_floor():
+		if Input.is_action_pressed("shoot"):
+			Engine.time_scale = 0.05
+
+	if Input.is_action_just_pressed("shoot"):
+		shoot_power = initial_shoot_power
 
 	if Input.is_action_pressed("shoot"):
-		pass
+		var shoot_direction = (get_global_mouse_position() - transform.get_origin()).normalized()
+		update_trajectory(0.005, get_real_velocity() + shoot_direction * shoot_power)
+		$Line2D.visible = true
+		shoot_power += load_speed * delta * (1 / Engine.time_scale)
 
 	if Input.is_action_just_released("shoot"):
+		$Line2D.visible = false
 		var x = parcel.instantiate();
 		get_tree().root.add_child(x)
 
 		x.transform = transform
 		var shoot_direction = (get_global_mouse_position() - transform.get_origin()).normalized()
-
 		x.linear_velocity = get_real_velocity() + shoot_direction * shoot_power
-		x.angular_velocity = shoot_power / 10.0
+		#x.angular_velocity = shoot_power / 10.0
 
+
+	print((1 / Engine.time_scale))
 
 
 
 	if not is_on_floor():
-		if Input.is_action_pressed("shoot"):
-			Engine.time_scale = 0.15
-
 		sliding = false
 
 	if sliding:

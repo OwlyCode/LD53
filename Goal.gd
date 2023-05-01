989931a3f09542feafaab92fd8cf9c7148f4f546
Tristan Maindron <tmaindron@gmail.com>
2023-05-01 21:26:19 +0200

    Inbox effects

--- Goal.gd
+++ Goal.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 var ending = false
 
+func _ready():
+	get_node("Inbox/InboxSprite").play("open")
+
 func _on_area_2d_body_entered(body:Node2D):
 	if "reached" in body and not ending:
 		body.reached = true
@@ -10,6 +13,8 @@ func _on_area_2d_body_entered(body:Node2D):
 		get_node("../../GenericUI").end_level()
 		$GoalSound.play()
 		get_tree().call_group("dog", "on_end_level")
+		get_node("Inbox/InboxSprite").play("closed")
+		shake_inbox()
 
 	if body.has_method("reach_goal"):
 		if body.reach_goal() and not ending:
@@ -18,3 +23,12 @@ func _on_area_2d_body_entered(body:Node2D):
 			get_node("../../GenericUI").end_level()
 			$GoalSound.play()
 			get_tree().call_group("dog", "on_end_level")
+			get_node("Inbox/InboxSprite").play("closed")
+			shake_inbox()
+
+func shake_inbox():
+	for i in range(5):
+		$Inbox.rotation_degrees -= 15
+		await get_tree().create_timer(0.1).timeout
+		$Inbox.rotation_degrees += 15
+		await get_tree().create_timer(0.1).timeout
